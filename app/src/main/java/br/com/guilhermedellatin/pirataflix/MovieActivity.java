c24c745aa34ae4144ea2f5ee90067ec7c2114584
GuilhermeDellatin <guidellatin@gmail.com>
2020-07-21 14:18:32 -0300

    Criação da nestedScrollView, nova recyclerview e criação do drawable play_button

--- app/src/main/java/br/com/guilhermedellatin/pirataflix/MovieActivity.java
+++ app/src/main/java/br/com/guilhermedellatin/pirataflix/MovieActivity.java
@@ -1,21 +1,42 @@
 package br.com.guilhermedellatin.pirataflix;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.LayerDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.guilhermedellatin.pirataflix.model.Movie;
 
 
 public class MovieActivity extends AppCompatActivity {
+
+    private TextView txtTitle;
+    private TextView txtDesc;
+    private TextView txtCast;
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie);
+
+        txtTitle = findViewById(R.id.text_view_title);
+        txtDesc = findViewById(R.id.text_view_desc);
+        txtCast = findViewById(R.id.text_view_cast);
+        recyclerView = findViewById(R.id.recycler_view_similar);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -23,6 +44,7 @@ public class MovieActivity extends AppCompatActivity {
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_24);
+            getSupportActionBar().setTitle(null);
         }
 
         LayerDrawable drawable = (LayerDrawable) ContextCompat.getDrawable(this, R.drawable.shadows);
@@ -31,6 +53,56 @@ public class MovieActivity extends AppCompatActivity {
             Drawable moviCover = ContextCompat.getDrawable(this, R.drawable.movie_4);
             drawable.setDrawableByLayerId(R.id.cover_drawable, moviCover);
             ((ImageView) findViewById(R.id.image_view_cover)).setImageDrawable(drawable);
+        }
+
+        txtTitle.setText("Batman Begins");
+        txtDesc.setText("Batman Begins Batman Begins Batman Begins Batman Begins Batman Begins Batman Begins Batman Begins Batman Begins Batman Begins");
+        txtCast.setText(getString(R.string.cast, "Christian Bale" + ",Michael Caine" + ",Liam Neeson" + ",Katie Holmes" + ",Gary Oldman"));
+
+        List<Movie> movies = new ArrayList<>();
+        for (int i = 0; i < 30; i++){
+            Movie movie = new Movie();
+            movies.add(movie);
+        }
+
+        recyclerView.setAdapter(new MovieAdapter(movies));
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 3));
+
+    }
+
+    private static class MovieHolder extends RecyclerView.ViewHolder{
+
+        final ImageView imageViewCover;
+
+        public MovieHolder(@NonNull View itemView) {
+            super(itemView);//ItemView será o container principal do layout dinamico que vamos criar, especifico da celula
+            imageViewCover= itemView.findViewById(R.id.image_view_cover);
+        }
+    }
+
+    private class MovieAdapter extends RecyclerView.Adapter<MovieHolder>{
+
+        private final List<Movie> movies;
+
+        private MovieAdapter(List<Movie> movies) {
+            this.movies = movies;
+        }
+
+        @NonNull
+        @Override
+        public MovieHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) { //Qual que é o layout xml que ele vai manipular, espera um ItemView
+            return new MovieHolder(getLayoutInflater().inflate(R.layout.movie_item, parent, false));
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull MovieHolder holder, int position) {//Sempre devolve um ViewHolder e a posição desse item
+            Movie movie = movies.get(position);
+            //holder.imageViewCover.setImageResource(movie.getCoverUrl());
+        }
+
+        @Override
+        public int getItemCount() { //Qual a quantidade de itens dessa coleção
+            return movies.size();
         }
     }
 
